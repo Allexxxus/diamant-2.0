7ac790c4770aac1e9c46cb23282fdeb4eeb68086
Allexxxus <crowly83@mail.ru>
2025-05-01 19:41:08 +0300

    tag in renamed to selectedTags

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Tags from '@/components/Tags';
 import Posts from '@/components/Posts';
 import { PageProps, Post, Tag } from '@/types';
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ tag: string[] }> }) {
+export default async function Page({ searchParams }: { searchParams: Promise<{ selectedTags: string[] }> }) {
 
   const params = await searchParams
 
@@ -11,15 +11,18 @@ export default async function Page({ searchParams }: { searchParams: Promise<{ t
   let posts: Post[] = [];
 
   let selectedTags: string[] = []
-  if (params.tag === undefined) {
+  if (params.selectedTags === undefined) {
+    console.log('selected tags: ' + selectedTags)
     selectedTags = []
-  } else if (typeof params.tag === 'string') {
-    selectedTags = [params.tag]
-  } else if (Array.isArray(params.tag)) {
-    selectedTags = params.tag
+  } else if (typeof params.selectedTags === 'string') {
+    console.log('selected tags: ' + selectedTags)
+    selectedTags = [params.selectedTags]
+  } else if (Array.isArray(params.selectedTags)) {
+    console.log('selected tags: ' + selectedTags)
+    selectedTags = params.selectedTags
   }
 
-  if (selectedTags !== undefined) {
+  if (selectedTags.length > 0) {
     posts = await loadPostsByTags(selectedTags)
   } else {
     posts = await loadAllPosts();
@@ -32,66 +35,3 @@ export default async function Page({ searchParams }: { searchParams: Promise<{ t
     </div>
   );
 }
-
-///////////////////////////////////////////////////////////
-
-// import { loadAllPosts, loadAllTags, loadPostsByTags } from '@/utils/actions';
-// import Tags from '@/components/Tags';
-// import Posts from '@/components/Posts';
-// import { PageProps, Post, Tag } from '@/types';
-
-// export default async function Page({ searchParams }: { searchParams: Promise<PageProps> }) {
-
-//   const selectedTags = await searchParams;
-
-//   const tags: Tag[] = await loadAllTags();
-//   let posts: Post[] = [];
-
-//   if (selectedTags !== undefined) {
-//     posts = Array.isArray(selectedTags)
-//       ? await loadPostsByTags(selectedTags[0])
-//       : await loadPostsByTags(selectedTags.split(','))
-//   } else {
-//     posts = await loadAllPosts();
-//   }
-
-//   return (
-//     <div>
-//       <Tags tags={tags} />
-//       <Posts posts={posts} />
-//     </div>
-//   );
-// }
-
-
-///////////////////////////////////////////////////////////
-
-
-// import { loadAllPosts, loadAllTags, loadPostsByTags } from '@/utils/actions';
-// import Tags from '@/components/Tags';
-// import Posts from '@/components/Posts';
-// import { PageProps, Post, Tag } from '@/types';
-
-// export default async function Page({ searchParams }: { searchParams: Record<string, string | string[] | undefined> }) {
-//   console.log(typeof searchParams);
-
-//   const { selectedTags } = await searchParams;
-
-//   const tags: Tag[] = await loadAllTags();
-//   let posts: Post[] = [];
-
-//   if (selectedTags !== undefined) {
-//     posts = Array.isArray(selectedTags)
-//       ? await loadPostsByTags(selectedTags[0].split(','))
-//       : await loadPostsByTags(selectedTags)
-//   } else {
-//     posts = await loadAllPosts();
-//   }
-
-//   return (
-//     <div>
-//       <Tags tags={tags} />
-//       <Posts posts={posts} />
-//     </div>
-//   );
-// }
