cf85ac99a96dc79313531f4b8b5f7f9db7397d1a
Allexxxus <crowly83@mail.ru>
2025-05-01 19:28:08 +0300

    searchParams' type is not Promise

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,24 @@ import Tags from '@/components/Tags';
 import Posts from '@/components/Posts';
 import { PageProps, Post, Tag } from '@/types';
 
-export default async function Page({ searchParams }: { searchParams: Record<string, string | string[] | undefined> }) {
+export default async function Page({ searchParams }: { searchParams: Promise<{ tag: string[] }> }) {
 
-  const { selectedTags } = await searchParams;
+  const params = await searchParams
 
   const tags: Tag[] = await loadAllTags();
   let posts: Post[] = [];
 
+  let selectedTags: string[] = []
+  if (params.tag === undefined) {
+    selectedTags = []
+  } else if (typeof params.tag === 'string') {
+    selectedTags = [params.tag]
+  } else if (Array.isArray(params.tag)) {
+    selectedTags = params.tag
+  }
+
   if (selectedTags !== undefined) {
-    posts = Array.isArray(selectedTags)
-      ? await loadPostsByTags(selectedTags[0])
-      : await loadPostsByTags(selectedTags.split(','))
+    posts = await loadPostsByTags(selectedTags)
   } else {
     posts = await loadAllPosts();
   }
@@ -25,6 +32,40 @@ export default async function Page({ searchParams }: { searchParams: Record<stri
     </div>
   );
 }
+
+///////////////////////////////////////////////////////////
+
+// import { loadAllPosts, loadAllTags, loadPostsByTags } from '@/utils/actions';
+// import Tags from '@/components/Tags';
+// import Posts from '@/components/Posts';
+// import { PageProps, Post, Tag } from '@/types';
+
+// export default async function Page({ searchParams }: { searchParams: Promise<PageProps> }) {
+
+//   const selectedTags = await searchParams;
+
+//   const tags: Tag[] = await loadAllTags();
+//   let posts: Post[] = [];
+
+//   if (selectedTags !== undefined) {
+//     posts = Array.isArray(selectedTags)
+//       ? await loadPostsByTags(selectedTags[0])
+//       : await loadPostsByTags(selectedTags.split(','))
+//   } else {
+//     posts = await loadAllPosts();
+//   }
+
+//   return (
+//     <div>
+//       <Tags tags={tags} />
+//       <Posts posts={posts} />
+//     </div>
+//   );
+// }
+
+
+///////////////////////////////////////////////////////////
+
 
 // import { loadAllPosts, loadAllTags, loadPostsByTags } from '@/utils/actions';
 // import Tags from '@/components/Tags';
